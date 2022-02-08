1a21c564aa1a80a728f7162890e6402426acc3fe
DK-SunilKumarReddy <79986827+DK-SunilKumarReddy@users.noreply.github.com>
2022-02-08 21:01:57 +0530

    Update 211123 1-Selecting Fans.c

--- 211123 1-Selecting Fans.c
+++ 211123 1-Selecting Fans.c
@@ -19,15 +19,12 @@ int main()
                 p=a[j],a[j]=a[j+1],a[j+1]=p;
                 strcpy(k,s[j]),strcpy(s[j],s[j+1]),strcpy(s[j+1],k);
             }
+            else if(a[j]==a[j+1])
+            {
+                if(strcmp(s[j],s[j+1])>0)
+                strcpy(k,s[j]),strcpy(s[j],s[j+1]),strcpy(s[j+1],k);
+            }
     }
     for(i=0;i<t;i++)
-    {
-        if(a[i]>a[i+1])
-        printf("%s\n",s[i]);
-        else if(a[i]==a[i+1])
-        {
-            if(strcmp(s[i],s[i+1])<=0)
-                printf("%s\n",s[i]);
-        }
-    }
+    printf("%s\n",s[i]);
 }
